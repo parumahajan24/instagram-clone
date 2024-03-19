a6ffd2d4b4285e1e91db5c5ab34c7b637b7ab5c0
Parul Mahajan <parumahajan24@gmail.com>
2024-03-19 12:10:05 -0400

    changed image resolution

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import PostHeader from './PostHeader'
-import PostFooter from './PostFooter'
-import { Box, Image } from '@chakra-ui/react'
+import React from "react";
+import PostHeader from "./PostHeader";
+import PostFooter from "./PostFooter";
+import { Box, Image } from "@chakra-ui/react";
 
 const FeedPost = () => {
-  return <>
-  <PostHeader/>
-  <Box my={2}>
-    <Image src="/img1.png" alt="User Profile Photo"/>
-  </Box>
-  <PostFooter/>
-  </>
-}
+  return (
+    <>
+      <PostHeader />
+      <Box my={2} borderRadius={4} overflow={"hidden"}>
+        <Image src="/img1.png" alt="User Profile Photo" borderRadius={4} />
+      </Box>
+      <PostFooter />
+    </>
+  );
+};
 
-export default FeedPost
+export default FeedPost;
