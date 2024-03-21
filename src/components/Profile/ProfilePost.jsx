ae92d80817cbb318156fa4ad46761e044a6d6e0c
Parul Mahajan <parumahajan24@gmail.com>
2024-03-21 11:25:26 -0400

    Created Modal for Profile feed photo and added Comment component

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,5 +1,172 @@
-const ProfilePost = () => {
-  return <div>ProfilePost</div>;
+import {
+  Avatar,
+  Box,
+  Divider,
+  Flex,
+  GridItem,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { AiFillHeart } from "react-icons/ai";
+import { FaComment } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import Comment from "../Comment/Comment";
+
+// <Grid> container (from ProfilePosts.jsx) is the parent of <GridItem> which we will use in ProfilePost
+const ProfilePost = ({ img }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <GridItem
+        cursor={"pointer"}
+        borderRadius={4}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
+      >
+        {/* When we hover over the post/image, it shows no. of likes and comments
+          We are using Flex:
+           Opacity is 0 (invisible) initially
+           on hover it will be 1
+    */}
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={"blackAlpha.700"}
+          transition={"all 0.3s ease"}
+          zIndex={1}
+          justifyContent={"center"}
+        >
+          {/* inside flex - No. of likes and comments */}
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+              <AiFillHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
+
+        <Image
+          src={img}
+          alt="Uploaded photo"
+          w={"100%"}
+          h={"100%"}
+          objectFit={"cover"}
+        />
+      </GridItem>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered={true}
+        size={{ base: "3xl", md: "5xl" }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody bg={"black"} pb={5}>
+            {/* Flex with two children - left : image, right : content; */}
+            <Flex
+              gap={4}
+              w={{ base: "90%", sm: "70%", md: "full" }}
+              mx={"auto"}
+            >
+              {/* left child: image */}
+              <Box
+                borderRadius={4}
+                overflow={"hidden"}
+                border={"1px solid"}
+                color={"whiteAlpha.300"}
+                flex={1.5}
+              >
+                <Image src={img} alt="profile post " />
+              </Box>
+
+              {/*right child - Flex for right hand side content */}
+              <Flex
+                flex={1}
+                flexDirection={"column"}
+                px={10}
+                display={{ base: "none", md: "flex" }}
+              >
+                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                  <Flex alignItems={"center"} gap={2}>
+                    <Avatar
+                      src="/profilepic.png"
+                      size={"sm"}
+                      name="Parul Mahajan"
+                    />
+                    <Text fontWeight={"bold"} fontSize={12}>
+                      parulmahajan {"username"}
+                    </Text>
+                  </Flex>
+
+                  <Box
+                    _hover={{ bg: "whiteAplha.300", color: "red.600" }}
+                    borderRadius={4}
+                    p={1}
+                  >
+                    <MdDelete size={20} cursor="pointer" />
+                  </Box>
+                </Flex>
+                <Divider my={4} bg={"gray.500"} />
+
+                {/* Comments sections */}
+                <VStack
+                  w={"full"}
+                  alignItems={"start"}
+                  maxH={"350px"}
+                  overflowY={"auto"}
+                >
+                  <Comment
+                    createdAt="1d ago"
+                    username={"parulmahajan username"}
+                    profilePic={"/profilepic.png"}
+                    text={"Dummy image"}
+                  />
+                   <Comment
+                    createdAt="12h ago"
+                    username={"viraat username"}
+                    profilePic={"/profilepic.png"}
+                    text={"Nice pic"}
+                  />
+                   <Comment
+                    createdAt="3h ago"
+                    username={"udit username"}
+                    profilePic={"/profilepic.png"}
+                    text={"WOW!"}
+                  />
+                </VStack>
+              </Flex>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default ProfilePost;
--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,3 +1,45 @@
+import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ProfilePost from "./ProfilePost";
+
 export const ProfilePosts = () => {
-  return <div>ProfilePosts</div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <Grid
+      templateColumns={{
+        sm: "repeat(1, 1fr)",
+        md: "repeat(3, 1fr)",
+      }}
+      gap={1}
+      columnGap={1}
+    >
+      {isLoading &&
+        [0, 1, 2, 3, 4, 5].map((_, index) => (
+          // loading skeletons display
+          <VStack key={index} alignItems={"flex-start"} gap={4}>
+            <Skeleton w={"full"}>
+              <Box h="300px">
+                wrapp the content- it will not be shown on screens
+              </Box>
+            </Skeleton>
+          </VStack>
+        ))}
+
+      {!isLoading && (
+        <>
+          <ProfilePost img="/img3.png" />
+          <ProfilePost img="/img6.png" />
+          <ProfilePost img="/img4.png" />
+          <ProfilePost img="/img2.png" />
+        </>
+      )}
+    </Grid>
+  );
 };
